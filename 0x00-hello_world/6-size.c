161e7a8814f837100be06e5c746b393e60ded377
Joykan <joykandie07@gmail.com>
2023-02-16 08:19:09 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 
+/**
+* main - prints the size of various types
+* Return: 0 if exited properly, non-zero otherwise
+*/
 int main(void)
 {
 int a;long int b;long long int c;char d;float f;
